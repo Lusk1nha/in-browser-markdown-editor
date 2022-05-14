c4a5a125ab0ed86fdc080a0d313364c19febd401
unknown <lucaspedro517@gmail.com>
2022-05-14 14:21:32 -0300

    First commit, adding styled-components and creating globalStyles

---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,10 @@
+import { Header, Navbar } from "./style";
+
+export function Nav() {
+  
+  return (
+    <Header>
+      <Navbar />
+    </Header>
+  );
+}
---
+++ src/components/Nav/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  background-color: black;
+  width: 100%;
+  min-height: 50px;
+`;
+
+export const Navbar = styled.nav`
+  width: 100%;
+`;
